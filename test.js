bf2e448a8ede2933d4f38a5c1d92e35884c435ca
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import reduceValues from "./source"
+import reduceWithValueKey from "./source"
 
 test(({equal, end}) => {
   equal(
