d5ff2e57ad596df94b23da13282992509259c8a9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:26:26 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,67 +1,45 @@
-import type from "@unction/type"
-import {scan} from "most"
-import fromFunctorToPairs from "@unction/fromfunctortopairs"
-
-import type {UnaryFunctionType} from "types"
-import type {StringType} from "types"
-import type {FunctorType} from "types"
-import type {ValueType} from "types"
-import type {ArrayKeyType} from "types"
-import type {ObjectKeyType} from "types"
-import type {MapKeyType} from "types"
-import type {KeyType} from "types"
-
-export default function reduceWithValueKey (reducer: mixed => ValueType => (KeyType | void) => mixed): UnaryFunctionType {
-  return function reduceWithValueKeyUnction (initial: mixed): UnaryFunctionType {
-    return function reduceWithValueKeyUnctionInitial (functor: FunctorType): mixed {
+import type from "@unction/type";
+import { scan } from "most";
+import fromFunctorToPairs from "@unction/fromfunctortopairs";
+export default function reduceWithValueKey(reducer) {
+  return function reduceWithValueKeyUnction(initial) {
+    return function reduceWithValueKeyUnctionInitial(functor) {
       switch (type(functor)) {
-        case "Array": {
-          return functor.reduce(
-            (accumulated: mixed, value: ValueType, key: ArrayKeyType): mixed =>
-              reducer(accumulated)(value)(key),
-            initial
-          )
-        }
-        case "Object": {
-          return fromFunctorToPairs(functor).reduce(
-            (accumulated: mixed, [key, value]: [ObjectKeyType, ValueType]): mixed =>
-              reducer(accumulated)(value)(key),
-            initial
-          )
-        }
-        case "Set": {
-          return fromFunctorToPairs(functor).reduce(
-            (accumulated: mixed, [, value]: [null, ValueType]): mixed =>
-              reducer(accumulated)(value)(),
-            initial
-          )
-        }
-        case "Map": {
-          return fromFunctorToPairs(functor).reduce(
-            (accumulated: mixed, [key, value]: [MapKeyType, ValueType]): mixed =>
-              reducer(accumulated)(value)(key),
-            initial
-          )
-        }
-        case "Stream": {
-          return scan(
-            (accumulated: mixed, value: ValueType): mixed =>
-              reducer(accumulated)(value)(),
-            initial,
-            functor
-          )
-        }
-        case "String": {
-          return fromFunctorToPairs(functor.split("")).reduce(
-            (accumulated: mixed, [key, value]: [ArrayKeyType, StringType]): mixed =>
-              reducer(accumulated)(value)(key),
-            initial
-          )
-        }
-        default: {
-          throw new Error(`reduceWithValueKey couldn't figure out how to reduce ${type(functor)}`)
-        }
+        case "Array":
+          {
+            return functor.reduce((accumulated, value, key) => reducer(accumulated)(value)(key), initial);
+          }
+
+        case "Object":
+          {
+            return fromFunctorToPairs(functor).reduce((accumulated, [key, value]) => reducer(accumulated)(value)(key), initial);
+          }
+
+        case "Set":
+          {
+            return fromFunctorToPairs(functor).reduce((accumulated, [, value]) => reducer(accumulated)(value)(), initial);
+          }
+
+        case "Map":
+          {
+            return fromFunctorToPairs(functor).reduce((accumulated, [key, value]) => reducer(accumulated)(value)(key), initial);
+          }
+
+        case "Stream":
+          {
+            return scan((accumulated, value) => reducer(accumulated)(value)(), initial, functor);
+          }
+
+        case "String":
+          {
+            return fromFunctorToPairs(functor.split("")).reduce((accumulated, [key, value]) => reducer(accumulated)(value)(key), initial);
+          }
+
+        default:
+          {
+            throw new Error(`reduceWithValueKey couldn't figure out how to reduce ${type(functor)}`);
+          }
       }
-    }
-  }
+    };
+  };
 }
